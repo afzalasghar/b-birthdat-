a215d46469a7ed90dcc8f9066fc15f5fcf464340
afzalasghar <itxafzal66@gmail.com>
2026-02-02 00:51:29 +0500

    Add files via upload

---
+++ bd/script.js
@@ -0,0 +1,9 @@
+function openGift() {
+  window.location.href = "gift.html";
+}
+
+
+ function showSurprise() {
+  document.querySelector(".board").classList.add("show");
+  document.querySelector(".kiss").classList.add("show");
+}
